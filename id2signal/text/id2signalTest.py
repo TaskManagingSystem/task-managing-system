88655fa1920154813e28e869d790d19aad05884e
TaskManagingSystem <115967532+TaskManagingSystem@users.noreply.github.com>
2022-10-24 01:55:57 +0900

    Add files via upload

---
+++ id2signal/text/id2signalTest.py
@@ -0,0 +1,307 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+# <rtc-template block="description">
+"""
+ @file id2signalTest.py
+ @brief ChangeIdIntoSignal
+ @date $Date$
+
+
+"""
+# </rtc-template>
+
+from __future__ import print_function
+import sys
+import time
+sys.path.append(".")
+
+# Import RTM module
+import RTC
+import OpenRTM_aist
+
+
+# Import Service implementation class
+# <rtc-template block="service_impl">
+
+import id2signal
+
+# </rtc-template>
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
+
+
+# This module's spesification
+# <rtc-template block="module_spec">
+id2signaltest_spec = ["implementation_id", "id2signalTest", 
+         "type_name",         "id2signalTest", 
+         "description",       "ChangeIdIntoSignal", 
+         "version",           "1.0.0", 
+         "vendor",            "HarutoFuruyama", 
+         "category",          "Converter", 
+         "activity_type",     "STATIC", 
+         "max_instance",      "1", 
+         "language",          "Python", 
+         "lang_type",         "SCRIPT",
+         "conf.default.machine_id", "0",
+
+         "conf.__widget__.machine_id", "text",
+
+         "conf.__type__.machine_id", "string",
+
+         ""]
+# </rtc-template>
+
+# <rtc-template block="component_description">
+##
+# @class id2signalTest
+# @brief ChangeIdIntoSignal
+# 
+# 
+# </rtc-template>
+class id2signalTest(OpenRTM_aist.DataFlowComponentBase):
+    
+    ##
+    # @brief constructor
+    # @param manager Maneger Object
+    # 
+    def __init__(self, manager):
+        OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+
+        self._d_signal_out = OpenRTM_aist.instantiateDataType(RTC.TimedBoolean)
+        """
+        """
+        self._signal_outIn = OpenRTM_aist.InPort("signal_out", self._d_signal_out)
+        self._d_latest_task = OpenRTM_aist.instantiateDataType(RTC.TimedString)
+        """
+        """
+        self._latest_taskOut = OpenRTM_aist.OutPort("latest_task", self._d_latest_task)
+
+
+        
+
+
+        # initialize of configuration-data.
+        # <rtc-template block="init_conf_param">
+        """
+        
+         - Name:  machine_id
+         - DefaultValue: 0
+        """
+        self._machine_id = ['0']
+        
+        # </rtc-template>
+
+
+         
+    ##
+    #
+    # The initialize action (on CREATED->ALIVE transition)
+    # 
+    # @return RTC::ReturnCode_t
+    # 
+    #
+    def onInitialize(self):
+        # Bind variables and configuration variable
+        self.bindParameter("machine_id", self._machine_id, "0")
+        
+        # Set InPort buffers
+        self.addInPort("signal_out",self._signal_outIn)
+        
+        # Set OutPort buffers
+        self.addOutPort("latest_task",self._latest_taskOut)
+        
+        # Set service provider to Ports
+        
+        # Set service consumers to Ports
+        
+        # Set CORBA Service Ports
+        
+        return RTC.RTC_OK
+    
+    ###
+    ## 
+    ## The finalize action (on ALIVE->END transition)
+    ## 
+    ## @return RTC::ReturnCode_t
+    #
+    ## 
+    #def onFinalize(self):
+    #
+    #    return RTC.RTC_OK
+    
+    #    ##
+    ##
+    ## The startup action when ExecutionContext startup
+    ## 
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onStartup(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The shutdown action when ExecutionContext stop
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onShutdown(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ##
+    #
+    # The activated action (Active state entry action)
+    #
+    # @param ec_id target ExecutionContext Id
+    # 
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onActivated(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+        ##
+    #
+    # The deactivated action (Active state exit action)
+    #
+    # @param ec_id target ExecutionContext Id
+    #
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onDeactivated(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+    ##
+    #
+    # The execution action that is invoked periodically
+    #
+    # @param ec_id target ExecutionContext Id
+    #
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onExecute(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The aborting action when main logic error occurred.
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    #    #
+    ##
+    #def onAborting(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The error action in ERROR state
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onError(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The reset action that is invoked resetting
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onReset(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The state update action that is invoked after onExecute() action
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+
+    ##
+    #def onStateUpdate(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The action that is invoked when execution context's rate is changed
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onRateChanged(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    def runTest(self):
+        return True
+
+def RunTest():
+    manager = OpenRTM_aist.Manager.instance()
+    comp = manager.getComponent("id2signalTest0")
+    if comp is None:
+        print('Component get failed.', file=sys.stderr)
+        return False
+    return comp.runTest()
+
+def id2signalTestInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=id2signaltest_spec)
+    manager.registerFactory(profile,
+                            id2signalTest,
+                            OpenRTM_aist.Delete)
+
+def MyModuleInit(manager):
+    id2signalTestInit(manager)
+    id2signal.id2signalInit(manager)
+
+    # Create a component
+    comp = manager.createComponent("id2signalTest")
+
+def main():
+    mgr = OpenRTM_aist.Manager.init(sys.argv)
+    mgr.setModuleInitProc(MyModuleInit)
+    mgr.activateManager()
+    mgr.runManager(True)
+
+    ret = RunTest()
+    mgr.shutdown()
+
+    if ret:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
